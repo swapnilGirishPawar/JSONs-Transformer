acfea3be3923d08a9a3c479f09fa821f7554a380
swapnilGirishPawar <swapnil.girish@anywhere.co>
2025-06-09 17:58:30 +0530

    added other components

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import logo from '../../assets/json-logo.jpg'
+
+const About = () => {
+  return (
+    <div className='about'>
+        <div className="about-left">
+            <img src={logo} alt="" />
+        </div>
+        <div className="about-right"></div>
+    </div>
+  )
+}
+
+export default About
